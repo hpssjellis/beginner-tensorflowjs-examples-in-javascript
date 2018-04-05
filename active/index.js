8182502a70676505cc6822208d6e7694046f58db
Jeremy Ellis <keyfreemusic@gmail.com>
2018-04-05 13:22:32 -0700

    Update index.js

--- active/index.js
+++ active/index.js
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
-import * as tf from '@tensorflow/tfjs';
+//import * as tf from '@tensorflow/tfjs';
 
 import * as data from './data';
 import * as loader from './loader';
