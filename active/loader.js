a623fce4f3f2e82bbe83d44c300b48ac5b78e257
Jeremy Ellis <keyfreemusic@gmail.com>
2018-04-05 13:22:48 -0700

    Update loader.js

--- active/loader.js
+++ active/loader.js
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
-import * as tf from '@tensorflow/tfjs';
+//import * as tf from '@tensorflow/tfjs';
 import * as ui from './ui';
 
 /**
