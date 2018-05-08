49052f0e4591465db55c66e73cd9a798bd45258b
Jeremy Ellis <keyfreemusic@gmail.com>
2018-05-07 23:35:14 -0700

    Update coco.js

--- tf-examples/posenet/coco/coco.js
+++ tf-examples/posenet/coco/coco.js
@@ -62,7 +62,7 @@ function drawResults(canvas, poses,
 
 const imageBucket = 'https://storage.googleapis.com/tfjs-models/assets/posenet/';
 
-async function loadImage(imagePath) {
+async function loadImage(image.src) {
   const image = new Image();
   const promise = new Promise((resolve, reject) => {
     image.crossOrigin = '';
