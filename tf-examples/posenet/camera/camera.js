5fe3cf6fb3269875ef9561eaeb68721dc7a84782
Jeremy Ellis <keyfreemusic@gmail.com>
2018-05-09 14:34:31 -0700

    Update camera.js

--- tf-examples/posenet/camera/camera.js
+++ tf-examples/posenet/camera/camera.js
@@ -198,7 +198,16 @@ function detectPoseInRealTime(video, net) {
       const pose = await guiState.net.estimateSinglePose(video, imageScaleFactor, flipHorizontal, outputStride);
       poses.push(pose);
         
-      document.getElementById('myDiv01').value = await JSON.stringify(pose, null, 3)
+        
+   //   if (){
+          
+      //    } else { 
+   
+         document.getElementById('myDiv01').value = await pose   /// JSON.stringify(pose, null, 3)
+     //  }  
+        
+        
+        
         
       minPoseConfidence = Number(
         guiState.singlePoseDetection.minPoseConfidence);
@@ -212,7 +221,15 @@ function detectPoseInRealTime(video, net) {
         guiState.multiPoseDetection.nmsRadius);
 
        
-      document.getElementById('myDiv01').value = await JSON.stringify(poses, null, 3)
+        
+        
+        
+        
+      document.getElementById('myDiv01').value = await  JSON.stringify(poses, null, 3)
+        
+        
+        
+        
         
       minPoseConfidence = Number(guiState.multiPoseDetection.minPoseConfidence);
       minPartConfidence = Number(guiState.multiPoseDetection.minPartConfidence);
