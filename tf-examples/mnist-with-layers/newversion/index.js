9f343fe75e7010d8b6275d172c334cb7e59e4b15
Jeremy Ellis <keyfreemusic@gmail.com>
2018-09-18 15:39:56 -0700

    Update index.js

--- tf-examples/mnist-with-layers/newversion/index.js
+++ tf-examples/mnist-with-layers/newversion/index.js
@@ -22,112 +22,235 @@
 
 
 
+/**
+ * Creates a convolutional neural network (Convnet) for the MNIST data.
+ *
+ * @returns {tf.Model} An instance of tf.Model.
+ */
+function createConvModel() {
+  // Create a sequential neural network model. tf.sequential provides an API
+  // for creating "stacked" models where the output from one layer is used as
+  // the input to the next layer.
+  const model = tf.sequential();
 
+  // The first layer of the convolutional neural network plays a dual role:
+  // it is both the input layer of the neural network and a layer that performs
+  // the first convolution operation on the input. It receives the 28x28 pixels
+  // black and white images. This input layer uses 16 filters with a kernel size
+  // of 5 pixels each. It uses a simple RELU activation function which pretty
+  // much just looks like this: __/
+  model.add(tf.layers.conv2d({
+    inputShape: [IMAGE_H, IMAGE_W, 1],
+    kernelSize: 3,
+    filters: 16,
+    activation: 'relu'
+  }));
 
-const model = tf.sequential();
+  // After the first layer we include a MaxPooling layer. This acts as a sort of
+  // downsampling using max values in a region instead of averaging.
+  // https://www.quora.com/What-is-max-pooling-in-convolutional-neural-networks
+  model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
 
-model.add(tf.layers.conv2d({
-  inputShape: [28, 28, 1],
-  kernelSize: 5,
-  filters: 8,
-  strides: 1,
-  activation: 'relu',
-  kernelInitializer: 'varianceScaling'
-}));
-model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-model.add(tf.layers.conv2d({
-  kernelSize: 5,
-  filters: 16,
-  strides: 1,
-  activation: 'relu',
-  kernelInitializer: 'varianceScaling'
-}));
-model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
-model.add(tf.layers.flatten());
-model.add(tf.layers.dense(
-    {units: 10, kernelInitializer: 'varianceScaling', activation: 'softmax'}));
+  // Our third layer is another convolution, this time with 32 filters.
+  model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
 
-const LEARNING_RATE = 0.15;
-const optimizer = tf.train.sgd(LEARNING_RATE);
-model.compile({
-  optimizer: optimizer,
-  loss: 'categoricalCrossentropy',
-  metrics: ['accuracy'],
-});
+  // Max pooling again.
+  model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
 
-const BATCH_SIZE = 64;
-const TRAIN_BATCHES = 150;
+  // Add another conv2d layer.
+  model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
 
-// Every few batches, test accuracy over many examples. Ideally, we'd compute
-// accuracy over the whole test set, but for performance we'll use a subset.
-const TEST_BATCH_SIZE = 1000;
-const TEST_ITERATION_FREQUENCY = 5;
+  // Now we flatten the output from the 2D filters into a 1D vector to prepare
+  // it for input into our last layer. This is common practice when feeding
+  // higher dimensional data to a final classification output layer.
+  model.add(tf.layers.flatten({}));
 
-async function train() {
-  ui.isTraining();
+  model.add(tf.layers.dense({units: 64, activation: 'relu'}));
 
-  const lossValues = [];
-  const accuracyValues = [];
+  // Our last layer is a dense layer which has 10 output units, one for each
+  // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9). Here the classes actually
+  // represent numbers, but it's the same idea if you had classes that
+  // represented other entities like dogs and cats (two output classes: 0, 1).
+  // We use the softmax function as the activation for the output layer as it
+  // creates a probability distribution over our 10 classes so their output
+  // values sum to 1.
+  model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
 
-  for (let i = 0; i < TRAIN_BATCHES; i++) {
-    const batch = data.nextTrainBatch(BATCH_SIZE);
-
-    let testBatch;
-    let validationData;
-    // Every few batches test the accuracy of the mode.
-    if (i % TEST_ITERATION_FREQUENCY === 0) {
-      testBatch = data.nextTestBatch(TEST_BATCH_SIZE);
-      validationData = [
-        testBatch.xs.reshape([TEST_BATCH_SIZE, 28, 28, 1]), testBatch.labels
-      ];
-    }
-
-    // The entire dataset doesn't fit into memory so we call fit repeatedly
-    // with batches.
-    const history = await model.fit(
-        batch.xs.reshape([BATCH_SIZE, 28, 28, 1]), batch.labels,
-        {batchSize: BATCH_SIZE, validationData, epochs: 1});
-
-    const loss = history.history.loss[0];
-    const accuracy = history.history.acc[0];
-
-    // Plot loss / accuracy.
-    lossValues.push({'batch': i, 'loss': loss, 'set': 'train'});
-    ui.plotLosses(lossValues);
-
-    if (testBatch != null) {
-      accuracyValues.push({'batch': i, 'accuracy': accuracy, 'set': 'train'});
-      ui.plotAccuracies(accuracyValues);
-    }
-
-    batch.xs.dispose();
-    batch.labels.dispose();
-    if (testBatch != null) {
-      testBatch.xs.dispose();
-      testBatch.labels.dispose();
-    }
-
-    await tf.nextFrame();
-  }
+  return model;
 }
 
-async function showPredictions() {
-  //model.summary()
-  
-  
-  
+/**
+ * Creates a model consisting of only flatten, dense and dropout layers.
+ *
+ * The model create here has approximately the same number of parameters
+ * (~31k) as the convnet created by `createConvModel()`, but is
+ * expected to show a significantly worse accuracy after training, due to the
+ * fact that it doesn't utilize the spatial information as the convnet does.
+ *
+ * This is for comparison with the convolutional network above.
+ *
+ * @returns {tf.Model} An instance of tf.Model.
+ */
+function createDenseModel() {
+  const model = tf.sequential();
+  model.add(tf.layers.flatten({inputShape: [IMAGE_H, IMAGE_W, 1]}));
+  model.add(tf.layers.dense({units: 42, activation: 'relu'}));
+  model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+  return model;
+}
+
+/**
+ * Compile and train the given model.
+ *
+ * @param {*} model The model to
+ */
+async function train(model) {
+  ui.logStatus('Training model...');
+
+  // Now that we've defined our model, we will define our optimizer. The
+  // optimizer will be used to optimize our model's weight values during
+  // training so that we can decrease our training loss and increase our
+  // classification accuracy.
+
+  // The learning rate defines the magnitude by which we update our weights each
+  // training step. The higher the value, the faster our loss values converge,
+  // but also the more likely we are to overshoot optimal parameters
+  // when making an update. A learning rate that is too low will take too long
+  // to find optimal (or good enough) weight parameters while a learning rate
+  // that is too high may overshoot optimal parameters. Learning rate is one of
+  // the most important hyperparameters to set correctly. Finding the right
+  // value takes practice and is often best found empirically by trying many
+  // values.
+  const LEARNING_RATE = 0.01;
+
+  // We are using rmsprop as our optimizer.
+  // An optimizer is an iterative method for minimizing an loss function.
+  // It tries to find the minimum of our loss function with respect to the
+  // model's weight parameters.
+  const optimizer = 'rmsprop';
+
+  // We compile our model by specifying an optimizer, a loss function, and a
+  // list of metrics that we will use for model evaluation. Here we're using a
+  // categorical crossentropy loss, the standard choice for a multi-class
+  // classification problem like MNIST digits.
+  // The categorical crossentropy loss is differentiable and hence makes
+  // model training possible. But it is not amenable to easy interpretation
+  // by a human. This is why we include a "metric", namely accuracy, which is
+  // simply a measure of how many of the examples are classified correctly.
+  // This metric is not differentiable and hence cannot be used as the loss
+  // function of the model.
+  model.compile({
+    optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  // Batch size is another important hyperparameter. It defines the number of
+  // examples we group together, or batch, between updates to the model's
+  // weights during training. A value that is too low will update weights using
+  // too few examples and will not generalize well. Larger batch sizes require
+  // more memory resources and aren't guaranteed to perform better.
+  const batchSize = 64;
+
+  // Leave out the last 15% of the training data for validation, to monitor
+  // overfitting during training.
+  const validationSplit = 0.15;
+
+  // Get number of training epochs from the UI.
+  const trainEpochs = ui.getTrainEpochs();
+
+  // We'll keep a buffer of loss and accuracy values over time.
+  let trainBatchCount = 0;
+
+  const trainData = data.getTrainData();
+  const testData = data.getTestData();
+
+  const totalNumBatches =
+      Math.ceil(trainData.xs.shape[0] * (1 - validationSplit) / batchSize) *
+      trainEpochs;
+
+  // During the long-running fit() call for model training, we include
+  // callbacks, so that we can plot the loss and accuracy values in the page
+  // as the training progresses.
+  let valAcc;
+  await model.fit(trainData.xs, trainData.labels, {
+    batchSize,
+    validationSplit,
+    epochs: trainEpochs,
+    callbacks: {
+      onBatchEnd: async (batch, logs) => {
+        trainBatchCount++;
+        ui.logStatus(
+            `Training... (` +
+            `${(trainBatchCount / totalNumBatches * 100).toFixed(1)}%` +
+            ` complete). To stop training, refresh or close page.`);
+        ui.plotLoss(trainBatchCount, logs.loss, 'train');
+        ui.plotAccuracy(trainBatchCount, logs.acc, 'train');
+        await tf.nextFrame();
+      },
+      onEpochEnd: async (epoch, logs) => {
+        valAcc = logs.val_acc;
+        ui.plotLoss(trainBatchCount, logs.val_loss, 'validation');
+        ui.plotAccuracy(trainBatchCount, logs.val_acc, 'validation');
+        await tf.nextFrame();
+      }
+    }
+  });
+
+  const testResult = model.evaluate(testData.xs, testData.labels);
+  const testAccPercent = testResult[1].dataSync()[0] * 100;
+  const finalValAccPercent = valAcc * 100;
+  ui.logStatus(
+      `Final validation accuracy: ${finalValAccPercent.toFixed(1)}%; ` +
+      `Final test accuracy: ${testAccPercent.toFixed(1)}%`);
+}
+
+/**
+ * Show predictions on a number of test examples.
+ *
+ * @param {tf.Model} model The model to be used for making the predictions.
+ */
+async function showPredictions(model) {
   const testExamples = 100;
-  const batch = data.nextTestBatch(testExamples);
+  const examples = data.getTestData(testExamples);
 
+  // Code wrapped in a tf.tidy() function callback will have their tensors freed
+  // from GPU memory after execution without having to call dispose().
+  // The tf.tidy callback runs synchronously.
   tf.tidy(() => {
-    const output = model.predict(batch.xs.reshape([-1, 28, 28, 1]));
+    const output = model.predict(examples.xs);
 
+    // tf.argMax() returns the indices of the maximum values in the tensor along
+    // a specific axis. Categorical classification tasks like this one often
+    // represent classes as one-hot vectors. One-hot vectors are 1D vectors with
+    // one element for each output class. All values in the vector are 0
+    // except for one, which has a value of 1 (e.g. [0, 0, 0, 1, 0]). The
+    // output from model.predict() will be a probability distribution, so we use
+    // argMax to get the index of the vector element that has the highest
+    // probability. This is our prediction.
+    // (e.g. argmax([0.07, 0.1, 0.03, 0.75, 0.05]) == 3)
+    // dataSync() synchronously downloads the tf.tensor values from the GPU so
+    // that we can use them in our normal CPU JavaScript code
+    // (for a non-blocking version of this function, use data()).
     const axis = 1;
-    const labels = Array.from(batch.labels.argMax(axis).dataSync());
+    const labels = Array.from(examples.labels.argMax(axis).dataSync());
     const predictions = Array.from(output.argMax(axis).dataSync());
 
-    ui.showTestResults(batch, predictions, labels);
+    ui.showTestResults(examples, predictions, labels);
   });
+}
+
+function createModel() {
+  let model;
+  const modelType = ui.getModelTypeId();
+  if (modelType === 'ConvNet') {
+    model = createConvModel();
+  } else if (modelType === 'DenseNet') {
+    model = createDenseModel();
+  } else {
+    throw new Error(`Invalid model type: ${modelType}`);
+  }
+  return model;
 }
 
 let data;
@@ -136,9 +259,18 @@ async function load() {
   await data.load();
 }
 
-async function mnist() {
+// This is our main function. It loads the MNIST data, trains the model, and
+// then shows what the model predicted on unseen test data.
+ui.setTrainButtonCallback(async () => {
+  ui.logStatus('Loading MNIST data...');
   await load();
-  await train();
-  showPredictions();
-}
-mnist();
+
+  ui.logStatus('Creating model...');
+  const model = createModel();
+  model.summary();
+
+  ui.logStatus('Starting model training...');
+  await train(model);
+
+  showPredictions(model);
+});
