e0edaac6f0be09e404f319341d8b6cc6dce51ac5
Jeremy Ellis <keyfreemusic@gmail.com>
2018-11-07 10:53:45 -0800

    Update DenseNet121.py

--- saved-models/advanced-keras-saving/keras-to-tfjs/DenseNet121.py
+++ saved-models/advanced-keras-saving/keras-to-tfjs/DenseNet121.py
@@ -1,4 +1,4 @@
 import keras
 import tensorflowjs as tfjs
-vgg16 = keras.applications.densenet.DenseNet121()
-tfjs.converters.save_keras_model(vgg16,'vgg16-tfjs')
+tempModel = keras.applications.densenet.DenseNet121()
+tfjs.converters.save_keras_model(tempModel,'tfjs-model')
