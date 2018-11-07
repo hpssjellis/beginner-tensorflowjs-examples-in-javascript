421c179b36f0cbe1835c1ab4e62bb8dd42b47d4a
Jeremy Ellis <keyfreemusic@gmail.com>
2018-11-07 11:02:36 -0800

    Update and rename VGG19 to VGG19.py

--- saved-models/advanced-keras-saving/keras-to-tfjs/VGG19.py
+++ saved-models/advanced-keras-saving/keras-to-tfjs/VGG19.py
@@ -1,4 +1,4 @@
 import keras
 import tensorflowjs as tfjs
-tempModel = keras.applications.densenet.vgg19.VGG19()
+tempModel = keras.applications.vgg19.VGG19()
 tfjs.converters.save_keras_model(tempModel,'tfjs-model')
