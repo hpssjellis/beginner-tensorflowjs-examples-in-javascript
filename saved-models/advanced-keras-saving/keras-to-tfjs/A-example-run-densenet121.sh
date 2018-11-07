63c691db9a471002d8bcb5686b6b6c43db668e8b
Jeremy Ellis <keyfreemusic@gmail.com>
2018-11-07 11:19:56 -0800

    Update A-example-run-densenet121.sh

--- saved-models/advanced-keras-saving/keras-to-tfjs/A-example-run-densenet121.sh
+++ saved-models/advanced-keras-saving/keras-to-tfjs/A-example-run-densenet121.sh
@@ -4,13 +4,13 @@ source ~/virtual-tf/bin/activate
 
 
 
-echo "running python hello42.py from the rocksetta-examples folder"
+echo "running python DenseNet121.py"
 echo ""
 echo ""
 
 python DenseNet121.py
 
-echo"Thanks DeepLizard"
+echo "Thanks DeepLizard"
 echo "http://deeplizard.com/learn/video/Kc2_x6pBYGE"
 echo "https://www.youtube.com/watch?v=HEQDRWMK6yY&list=PLZbbT5o_s2xr83l8w44N_g3pygvajLrJ-"
 echo "Done......................"
