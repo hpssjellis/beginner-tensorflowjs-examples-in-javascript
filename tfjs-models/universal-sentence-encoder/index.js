f6f6624607f953f5a66d5c258206205e5c00029b
Jeremy Ellis <keyfreemusic@gmail.com>
2020-04-29 22:46:41 -0700

    Update index.js

--- tfjs-models/universal-sentence-encoder/index.js
+++ tfjs-models/universal-sentence-encoder/index.js
@@ -64,7 +64,9 @@ const init = async () => {
           sentenceI.matMul(sentenceJ, sentenceITranspose, sentenceJTransepose)
               .dataSync();
 
-      ctx.fillStyle = interpolateReds(score);
+     // ctx.fillStyle = interpolateReds(score);
+      ctx.fillStyle = d3.interpolateYlGn(score);
+   
       ctx.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
       ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
     }
